0a29d355f620013793d058527ff5d8037d498137
Dan Gurney <dan@dangurney.net>
2023-02-28 22:34:13 +0000

    feat: introspect db types

--- app/db.d.ts
+++ app/db.d.ts
@@ -5,32 +5,32 @@ export type Generated<T> = T extends ColumnType<infer S, infer I, infer U>
   : ColumnType<T, T | undefined, T>;
 
 export interface Events {
-  id: Generated<number>;
+  id: number;
   organizer_id: number;
   name: string;
   proposed_date: string;
   finalized_date: string | null;
-  created_at: Generated<Date>;
-  updated_at: Generated<Date>;
+  created_at: Generated<string>;
+  updated_at: Generated<string>;
 }
 
 export interface Invitees {
-  id: Generated<number>;
+  id: number;
   event_id: number;
   user_id: number;
   invite_sent: Generated<number>;
   response: string | null;
   finalized_date_sent: Generated<number>;
-  created_at: Generated<Date>;
-  updated_at: Generated<Date>;
+  created_at: Generated<string>;
+  updated_at: Generated<string>;
 }
 
 export interface Users {
-  id: Generated<number>;
+  id: number;
   email: string;
   hashed_password: string | null;
-  created_at: Generated<Date>;
-  updated_at: Generated<Date>;
+  created_at: Generated<string>;
+  updated_at: Generated<string>;
 }
 
 export interface DB {
